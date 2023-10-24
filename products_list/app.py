c09167cd26c75c47edfcf28601551ea5ca8d5193
CHADLIZakaria <zakaria.chadli05.com>
2023-10-24 01:16:57 +0100

    get reviews

--- products_list/app.py
+++ products_list/app.py
@@ -1,32 +1,64 @@
-from flask import Flask
-from flask_cors import CORS
-import json
+from os import abort
 
-app = Flask("List of products")
-CORS(app)
+from cloudant.client import Cloudant
+from cloudant.query import Query
+from flask import Flask, jsonify, request
+import atexit
 
-@app.route("/products")
-def getProductsList():
-  products = []
-  with open("products.json", "r") as prodfile:
-    data = json.load(prodfile)
-    for product in data["products"]:
-      products.append(product["product"])
-    return {"products":products}
+# Add your Cloudant service credentials here
+cloudant_username = '5b1e4785-227c-4ca4-91a9-1171b784ec06-bluemix'
+cloudant_api_key = 'NEfwRcdGaflNjSl3bJU-JDWTjmSnNCz7US6ZHauvaIFt'
+cloudant_url = 'https://5b1e4785-227c-4ca4-91a9-1171b784ec06-bluemix.cloudantnosqldb.appdomain.cloud'
+client = Cloudant.iam(cloudant_username, cloudant_api_key, connect=True, url=cloudant_url)
+session = client.session()
+print('Databases:', client.all_dbs())
+db = client['reviews']
+app = Flask(__name__)
 
-@app.route("/getdealers/<product>")
-def getDealers(product):
-  products = []
-  ret = False
-  with open("products.json", "r") as prodfile:
-    data = json.load(prodfile)
-    for productMeta in data["products"]:
-      if productMeta["product"] == product:
-        ret = True
-        return {"dealers":productMeta["Dealers"]}
-  if ret == False:
-    return {"message":"Could not find dealers for this product"}
 
-if __name__=="__main__":
+@app.route('/api/get_reviews', methods=['GET'])
+def get_reviews():
+    dealership_id = request.args.get('id')
+    # Check if "id" parameter is missing
+    if dealership_id is None:
+        return jsonify({"error": "Missing 'id' parameter in the URL"}), 400
+    # Convert the "id" parameter to an integer (assuming "id" should be an integer)
+    try:
+        dealership_id = int(dealership_id)
+    except ValueError:
+        return jsonify({"error": "'id' parameter must be an integer"}), 400
+    # Define the query based on the 'dealership' ID
+    selector = {
+        'dealership': dealership_id
+    }
+    # Execute the query using the query method
+    result = db.get_query_result(selector)
+    # Create a list to store the documents
+    data_list = []
+    # Iterate through the results and add documents to the list
+    for doc in result:
+        data_list.append(doc)
+    # Return the data as JSON
+    return jsonify(data_list)
+
+
+@app.route('/api/post_review', methods=['POST'])
+def post_review():
+    if not request.json:
+        abort(400, description='Invalid JSON data')
+
+    # Extract review data from the request JSON
+    review_data = request.json
+    # Validate that the required fields are present in the review data
+    required_fields = ['id', 'name', 'dealership', 'review', 'purchase', 'purchase_date', 'car_make', 'car_model',
+                       'car_year']
+    for field in required_fields:
+        if field not in review_data:
+            abort(400, description=f'Missing required field: {field}')
+    # Save the review data as a new document in the Cloudant database
+    db.create_document(review_data)
+    return jsonify({"message": "Review posted successfully"}), 201
+
+
+if __name__ == '__main__':
     app.run(debug=True)
-    # When no port is specified, starts at default port 5000
